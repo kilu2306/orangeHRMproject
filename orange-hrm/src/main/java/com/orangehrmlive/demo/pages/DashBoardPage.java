2c3bf4fd602affc757c812b8c6cc6e0a4bdd711e
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-05-10 19:15:33 +0100

    Update DashBoardPage.java

--- orange-hrm/src/main/java/com/orangehrmlive/demo/pages/DashBoardPage.java
+++ orange-hrm/src/main/java/com/orangehrmlive/demo/pages/DashBoardPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DashBoardPage extends Utility {
-    public void inIt(){
+    public DashBoardPage(){
         PageFactory.initElements(driver,this);
     }
     @FindBy(xpath = "//h1[normalize-space()='Dashboard']")

ad71955a0d876174564de57092e3f89a99ca3dc4
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-05-10 19:14:55 +0100

    Update AdminPage.java

--- orange-hrm/src/main/java/com/orangehrmlive/demo/pages/AdminPage.java
+++ orange-hrm/src/main/java/com/orangehrmlive/demo/pages/AdminPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AdminPage extends Utility {
-    public void inIt() {
+    public AdminPage() {
         PageFactory.initElements(driver, this);
     }
 

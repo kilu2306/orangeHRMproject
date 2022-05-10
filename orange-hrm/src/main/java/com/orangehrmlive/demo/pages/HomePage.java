0308f95c24e56d28ddeafa268d7eaea152c79266
kukii77 <100643358+kukii77@users.noreply.github.com>
2022-05-10 19:15:57 +0100

    Update HomePage.java

--- orange-hrm/src/main/java/com/orangehrmlive/demo/pages/HomePage.java
+++ orange-hrm/src/main/java/com/orangehrmlive/demo/pages/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends Utility {
-    public void inIt() {
+    public HomePage() {
         PageFactory.initElements(driver, this);
     }
 
